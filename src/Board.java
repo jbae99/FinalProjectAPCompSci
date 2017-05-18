4edbafc4f9c6f0691b2fb1329214ed9f6a361585
jbae99 <jbae99@gmail.com>
2017-05-18 08:53:31 -0700

    Added a layer for pictures

--- src/Board.java
+++ src/Board.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
@@ -17,21 +18,26 @@ public class Board {
     public static void board(Stage primaryStage) {
         primaryStage.setTitle("Tile Match");
 
+        StackPane rootPane = new StackPane();
 
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(1);
-        grid.setVgap(1);
-        grid.setPadding(new Insets(1,1,1,1));
+        GridPane btns = new GridPane();
+        btns.setAlignment(Pos.CENTER);
+        btns.setHgap(1);
+        btns.setVgap(1);
+        btns.setPadding(new Insets(1,1,1,1));
+        GridPane pics = new GridPane();
+        pics.setAlignment(Pos.CENTER);
+        pics.setHgap(1);
+        pics.setVgap(1);
+        pics.setPadding(new Insets(1,1,1,1));
 
+        rootPane.getChildren().addAll(pics, btns);
 
-
-        Scene scene = new Scene(grid, 900, 900);
+        Scene scene = new Scene(rootPane, 900, 900);
         primaryStage.setScene(scene);
 
-
-        HBox[][] hbList = new HBox[6][6];
+        
         Button[][] btnList = new Button[6][6];
 
         for (int i = 0; i <6; i++)
@@ -41,7 +47,7 @@ public class Board {
                 btnList[i][j] = new Button("Click Me!");
                 btnList[i][j].setPrefWidth(150);
                 btnList[i][j].setPrefHeight(150);
-                grid.add(btnList[i][j], i, j);
+                btns.add(btnList[i][j], i, j);
             }
         }
 

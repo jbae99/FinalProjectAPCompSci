9e1c7cffddf176d8d716cc7380da59579dc37c2e
kalynklimek <kaky000@gmail.com>
2017-06-08 09:21:08 -0700

    updated Runner

--- src/Runner.java
+++ src/Runner.java
@@ -3,36 +3,23 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.event.EventHandler;
-import javafx.animation.Timeline;
-import javafx.animation.KeyFrame;
+import javafx.beans.property.SimpleStringProperty;
 
 import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
-<<<<<<< HEAD
 
 public class Runner extends Application {
     long startTime;
-    public int timecounter;
-    private Label timerLabel = new Label();
-    private Timeline timeline;
+    long endTime;
     Timer t;
-    Duration secondsElapsed;
+    long timeElapsed;
+    private String[] split;
+    private SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+    public SimpleStringProperty min, sec, sspTime;
 
     public static void main(String[] args) {
-=======
-public class Runner extends Application{
-    long startTime;
-    public int timecounter;
-    Timer t;
-    TimerTask task;
-    Duration secondsElapsed;
-
-    public static void main(String[] args){
->>>>>>> origin/master
         launch(args);
     }
 
@@ -41,51 +28,42 @@ public class Runner extends Application{
 
         Board.board(primaryStage);
         startTime = System.currentTimeMillis();
-        timerLabel.setText(secondsElapsed.toString());
-
-        Button button = new Button();
-        button.setText("Start Timer");
-
-
-        Controller cntrl = new Controller();
 
         primaryStage.show();
-
-
     }
 
-<<<<<<< HEAD
-    public void timer(Timer t){
-=======
-
-    public void timer()
+    public Runner()
     {
         Timer t = new Timer();
->>>>>>> origin/master
-        timecounter = 0;
+        timeElapsed = 0;
+        min = new SimpleStringProperty("00");
+        sec = new SimpleStringProperty("00");
+        sspTime = new SimpleStringProperty("00:00");
     }
 
     public void run() {
-        setTimeCounter(getTimeCounter() + 1);
+        setTimeElapsed(getTimeElapsed() + 1);
     }
 
-    public int getTimeCounter() {
-        return timecounter;
+    public void setTimeElapsed(long timeElapsed)
+    {
+        this.timeElapsed = timeElapsed;
+        split = sdf.format(new Date(timeElapsed)).split(":");
+        min.set(split[0]);
+        sec.set(split[1]);
+        sspTime.set(min.get() + ":" + sec.get());
     }
 
-    public void setTimeCounter(int timecounter) {
-        this.timecounter = timecounter;
-
+    public long getTimeElapsed(){
+        return timeElapsed;
     }
 
-    public double duration()
+    public long duration()
     {
         t.cancel();
         long endTime = System.currentTimeMillis();
-        long deltaTime = endTime - startTime;
-        double secondsElapsed = deltaTime / 1000.0;
-        return secondsElapsed;
+        long timeElapsed = endTime - startTime;
+        return timeElapsed;
     }
-
 
 }
